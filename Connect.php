76f4a172c76f209324d298e6b36b370c8ce1bdcc
Nguyendinhnam110204 <nguyendinhnam204211@gmail.com>
2024-11-23 17:49:59 +0700

    Giao diện chính

---
+++ Connect.php
@@ -0,0 +1,7 @@
+<?php
+    $servername='localhost';
+    $user='root';
+    $pass='';
+    $db='QuanLyHoSoNhanSu';
+    $conn=mysqli_connect($servername,$user,$pass,$db);
+?>
---
+++ NhanSu_Index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<!--=== Coding by CodingLab | www.codinglabweb.com === -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!----======== CSS ======== -->
+    <link rel="stylesheet" href="./CSS/Admin_Style.css?v = <?php echo time(); ?>">
+     
+    <!----===== Iconscout CSS ===== -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
+    <title>Admin</title> 
+</head>
+<body>
+    <nav>
+        <div class="logo-name">
+            <div class="logo-image">
+                <img src="http://utt.edu.vn/home/images/stories/logo-utt-border.png" alt="">
+            </div>
+
+            <span class="logo_name" style="color: orange;">UTT SCHOOL</span>
+        </div>
+
+        <div class="menu-items">
+            <ul class="nav-links">
+            <li><a href="./NguoiDung/index_NguoiDung.php">
+                    <i class="uil uil-user"></i>
+                    <span class="link-name">Thông tin cá nhân</span>
+                </a></li>
+                <li><a href="#">
+                    <i class="uil uil-table"></i>
+                    <span class="link-name">Lịch sử công tác</span>
+                </a></li>
+                <li><a href="#">
+                    <i class="uil uil-book-reader"></i>
+                    <span class="link-name">Nghỉ phép</span>
+                </a></li>
+                <li><a href="#">
+                    <i class="uil uil-file-info-alt"></i>
+                    <span class="link-name">Lương</span>
+                </a></li>
+            </ul>
+            
+            <ul class="logout-mode">
+                <li><a href="./Login/DangXuat.php">
+                    <i class="uil uil-signout"></i>
+                    <span class="link-name">Đổi mật khẩu</span>
+                </a></li>
+
+                <li><a href="./Login/DangXuat.php">
+                    <i class="uil uil-signout"></i>
+                    <span class="link-name">Đăng xuất</span>
+                </a></li>
+
+                <li class="mode">
+                    <a href="#">
+                        <i class="uil uil-moon"></i>
+                    <span class="link-name">Chế độ</span>
+                </a>
+
+                <div class="mode-toggle">
+                  <span class="switch"></span>
+                </div>
+            </li>
+            </ul>
+        </div>
+    </nav>
+
+    <section class="dashboard">
+        <div class="top">
+            <i class="uil uil-bars sidebar-toggle"></i>
+
+            <div class="search-box">
+                <i class="uil uil-search"></i>
+                <input type="text" placeholder="Tìm kiếm...">
+            </div>
+
+            <img src="./Img/profile.jpg" alt="Avatar" style="margin-right: 50px;">
+
+
+            
+        </div>
+
+        <div class="dash-content">
+            
+        </div>
+    </section>
+
+    <script src="./JS/Admin_Script.js?v = <?php echo time(); ?>"></script>
+</body>
+</html>
